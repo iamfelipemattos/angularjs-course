5615d61b7deedb2b0de75852bd1aa53b66f0fcfe
Felipe Mattos <hi@felipemattos.io>
2015-10-16 08:39:47 -0300

    Adds sorting & ordering

--- section5/lesson1/main.js
+++ section5/lesson1/main.js
@@ -28,7 +28,15 @@ app.controller('PersonListController', function ($scope, ContactService) {
     };
 
     $scope.$watch('search', function (newVal, oldVal) {
-        console.log(newVal);
+        if (angular.isDefined(newVal)) {
+            $scope.contacts.doSearch(newVal);
+        }
+    })
+
+    $scope.$watch('order', function (newVal, oldVal) {
+        if (angular.isDefined(newVal)) {
+            $scope.contacts.doOrder(newVal);
+        }
     })
 
 });
@@ -44,12 +52,29 @@ app.service('ContactService', function (Contact) {
         , 'isLoading': false
         , 'selectedPerson': null
         , 'persons': []
+        , 'search': null
+        , 'doSearch': function (search) {
+            self.hasMore = true;
+            self.page = 1;
+            self.persons = [];
+            self.search = search;
+            self.loadContacts();
+        }
+        , 'doOrder': function (order) {
+            self.hasMore = true;
+            self.page = 1;
+            self.persons = [];
+            self.ordering = order;
+            self.loadContacts();
+        }
         , 'loadContacts': function () {
             if ( self.hasMore && !self.isLoading) {
                 self.isLoading = true;
 
                 var params = {
                     'page': self.page
+                  , 'search': self.search
+                  , 'ordering': self.ordering
                 };
 
                 Contact.get(params, function (data) {
