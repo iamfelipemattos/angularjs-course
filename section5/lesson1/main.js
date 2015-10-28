fea3a1893b719d7a34ba4236ded929b6ce30da23
Felipe Mattos <hi@felipemattos.io>
2015-10-27 23:56:52 -0200

    Update a contact using our resource

--- section5/lesson1/main.js
+++ section5/lesson1/main.js
@@ -15,11 +15,19 @@ app.config(function($httpProvider, $resourceProvider, laddaProvider) {
 });
 
 app.factory('Contact', function ($resource) {
-    return $resource("https://codecraftpro.com/api/samples/v1/contact/:id/");
+    return $resource("https://codecraftpro.com/api/samples/v1/contact/:id/", {id: '@id'}, {
+        update: {
+            method: 'PUT'
+        }
+    });
 });
 
 app.controller('PersonDetailController', function ($scope, ContactService)  {
     $scope.contacts = ContactService;
+
+    $scope.save = function () {
+        $scope.contacts.updateContact($scope.contacts.selectedPerson)
+    }
 });
 
 app.controller('PersonListController', function ($scope, ContactService) {
@@ -56,6 +64,7 @@ app.service('ContactService', function (Contact) {
           'page': 1
         , 'hasMore': true
         , 'isLoading': false
+        , 'isSaving': false
         , 'selectedPerson': null
         , 'persons': []
         , 'search': null
@@ -101,6 +110,13 @@ app.service('ContactService', function (Contact) {
                 self.page += 1;
                 self.loadContacts();
             }
+        },
+        'updateContact': function (person) {
+            console.log("Service Called Update");
+            self.isSaving = true;
+            person.$update().then(function () {
+                self.isSaving = false;
+            });
         }
     };
 
