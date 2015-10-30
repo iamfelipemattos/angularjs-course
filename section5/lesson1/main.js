a2ef32f26fcfd410330d7c049dd1fe38486b1e46
Felipe Mattos <hi@felipemattos.io>
2015-10-29 23:40:34 -0200

    Delete a contact using our resource

--- section5/lesson1/main.js
+++ section5/lesson1/main.js
@@ -27,6 +27,10 @@ app.controller('PersonDetailController', function ($scope, ContactService)  {
 
     $scope.save = function () {
         $scope.contacts.updateContact($scope.contacts.selectedPerson)
+    };
+
+     $scope.remove = function () {
+        $scope.contacts.removeContact($scope.contacts.selectedPerson)
     }
 });
 
@@ -116,6 +120,15 @@ app.service('ContactService', function (Contact) {
             self.isSaving = true;
             person.$update().then(function () {
                 self.isSaving = false;
+            });
+        },
+        'removeContact': function (person) {
+            self.isDeleting = true;
+            person.$remove().then(function () {
+                self.isDeleting = false;
+                var index = self.persons.indexOf(person);
+                self.persons.splice(index, 1);
+                self.selectedPerson = null;
             });
         }
     };
