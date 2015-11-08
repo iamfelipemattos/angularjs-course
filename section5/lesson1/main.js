c292d62f887f52b06bbf230d0d0405160516a16a
Felipe Mattos <hi@felipemattos.io>
2015-11-08 11:39:00 -0200

    Promises in AngularJS with the  service

--- section5/lesson1/main.js
+++ section5/lesson1/main.js
@@ -57,7 +57,9 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
 
     $scope.createContact = function () {
         console.log("createContact");
-        $scope.contacts.createContact($scope.contacts.selectedPerson);
+        $scope.contacts.createContact($scope.contacts.selectedPerson).then(function () {
+            $scope.createModal.hide();
+        })
     };
 
     $scope.$watch('search', function (newVal, oldVal) {
@@ -74,7 +76,7 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
 
 });
 
-app.service('ContactService', function (Contact) {
+app.service('ContactService', function (Contact, $q) {
 
     var self =  {
         'addPerson': function (person) {
@@ -147,10 +149,17 @@ app.service('ContactService', function (Contact) {
             });
         },
         'createContact': function (person) {
+            var d = $q.defer();
             self.isSaving = true;
             Contact.save(person).$promise.then(function () {
                 self.isSaving = false;
+                self.selectedPerson = null;
+                self.hasMore = true;
+                self.persons = [];
+                self.loadContacts();
+                d.resolve();
             });
+            return d.promise;
         }
     };
 
