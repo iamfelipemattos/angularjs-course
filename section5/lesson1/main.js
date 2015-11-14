9a4a3576ea1838ba139b0d7866fa33b77e4b8eef
Felipe Mattos <hi@felipemattos.io>
2015-11-14 21:44:04 -0200

    Adds toaster messages with angularjsToaster

--- section5/lesson1/main.js
+++ section5/lesson1/main.js
@@ -4,7 +4,9 @@ var app = angular.module('application', [
     'angularSpinner',
     'jcs-autoValidate',
     'angular-ladda',
-    'mgcrea.ngStrap'
+    'mgcrea.ngStrap',
+    'toaster',
+    'ngAnimate'
 ]);
 
 app.config(function($httpProvider, $resourceProvider, laddaProvider, $datepickerProvider) {
@@ -31,12 +33,12 @@ app.controller('PersonDetailController', function ($scope, ContactService)  {
     $scope.contacts = ContactService;
 
     $scope.save = function () {
-        $scope.contacts.updateContact($scope.contacts.selectedPerson)
+        $scope.contacts.updateContact($scope.contacts.selectedPerson);
     };
 
      $scope.remove = function () {
-        $scope.contacts.removeContact($scope.contacts.selectedPerson)
-    }
+        $scope.contacts.removeContact($scope.contacts.selectedPerson);
+    };
 });
 
 app.controller('PersonListController', function ($scope, $modal, ContactService) {
@@ -56,65 +58,65 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
             scope: $scope,
             template: 'templates/modal.html',
             show: true
-        })
+        });
     };
 
     $scope.createContact = function () {
         console.log("createContact");
         $scope.contacts.createContact($scope.contacts.selectedPerson).then(function () {
             $scope.createModal.hide();
-        })
+        });
     };
 
     $scope.$watch('search', function (newVal, oldVal) {
         if (angular.isDefined(newVal)) {
             $scope.contacts.doSearch(newVal);
         }
-    })
+    });
 
     $scope.$watch('order', function (newVal, oldVal) {
         if (angular.isDefined(newVal)) {
             $scope.contacts.doOrder(newVal);
         }
-    })
+    });
 
 });
 
-app.service('ContactService', function (Contact, $q) {
+app.service('ContactService', function (Contact, $q, toaster) {
 
     var self =  {
         'addPerson': function (person) {
             this.persons.push(person);
         },
-          'page': 1
-        , 'hasMore': true
-        , 'isLoading': false
-        , 'isSaving': false
-        , 'selectedPerson': null
-        , 'persons': []
-        , 'search': null
-        , 'doSearch': function (search) {
+          'page': 1,
+          'hasMore': true,
+          'isLoading': false,
+          'isSaving': false,
+          'selectedPerson': null,
+          'persons': [],
+          'search': null,
+          'doSearch': function (search) {
             self.hasMore = true;
             self.page = 1;
             self.persons = [];
             self.search = search;
             self.loadContacts();
-        }
-        , 'doOrder': function (order) {
+        },
+        'doOrder': function (order) {
             self.hasMore = true;
             self.page = 1;
             self.persons = [];
             self.ordering = order;
             self.loadContacts();
-        }
-        , 'loadContacts': function () {
+        },
+        'loadContacts': function () {
             if ( self.hasMore && !self.isLoading) {
                 self.isLoading = true;
 
                 var params = {
-                    'page': self.page
-                  , 'search': self.search
-                  , 'ordering': self.ordering
+                    'page': self.page,
+                    'search': self.search,
+                    'ordering': self.ordering
                 };
 
                 Contact.get(params, function (data) {
@@ -141,6 +143,7 @@ app.service('ContactService', function (Contact, $q) {
             self.isSaving = true;
             person.$update().then(function () {
                 self.isSaving = false;
+                toaster.pop('success', 'Updated ' + person.name);
             });
         },
         'removeContact': function (person) {
@@ -150,6 +153,7 @@ app.service('ContactService', function (Contact, $q) {
                 var index = self.persons.indexOf(person);
                 self.persons.splice(index, 1);
                 self.selectedPerson = null;
+                toaster.pop('success', 'Deleted ' + person.name);
             });
         },
         'createContact': function (person) {
@@ -161,6 +165,7 @@ app.service('ContactService', function (Contact, $q) {
                 self.hasMore = true;
                 self.persons = [];
                 self.loadContacts();
+                toaster.pop('success', 'Created ' + person.name);
                 d.resolve();
             });
             return d.promise;
