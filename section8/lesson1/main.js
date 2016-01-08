f8f076afdb29b742dba4e3c24e6389071173846f
Felipe Mattos <hi@felipemattos.io>
2016-01-07 23:16:38 -0200

    Isolate scope &

--- section8/lesson1/main.js
+++ section8/lesson1/main.js
@@ -96,7 +96,8 @@ app.directive('ccCard', function () {
         'restrict': 'AE',
         'templateUrl': 'templates/card.html',
         'scope': {
-            'user': '='
+            'user': '=',
+            'deleteUser': '&'
         }
     }
 });
@@ -145,6 +146,10 @@ app.controller('PersonListController', function ($scope, $modal, ContactService)
     $scope.loadMore = function () {
         console.log("Load More!!!");
         $scope.contacts.loadMore();
+    };
+
+    $scope.parentDeleteUser = function (user) {
+        $scope.contacts.removeContact(user);
     };
 
     $scope.showCreateModal = function () {
